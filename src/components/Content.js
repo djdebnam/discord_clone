ecf96b974c87a6be122dfb41af4dbe331f7a666d
ddebnam <djdebnam@gmail.com>
2022-06-27 09:17:22 -0400

    removed app folder

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,7 @@
 const Content = () => {
   return (
     <div id="container">
-        <h1>Content</h1>
+        <h1>Content 2</h1>
     </div>
   )
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,7 +5,9 @@ const SideBar = () => {
         <div id="side-bar">
             <button id="home-button"><FaHome id="home-button-icon"/></button>
             <div id="button-seperator"></div>
-            <button class="new-server-button"><FaPlus/></button>
+            <button class="server-button">D</button>
+            <button class="server-button">M</button> 
+            <button className="new-server-button"><FaPlus/></button>
             <div id="user-button-holder-primary"></div>
             <div id="user-button-holder-secondary"></div>
             <button id="user-button"><FaUser id="user-button-icon"/></button>
