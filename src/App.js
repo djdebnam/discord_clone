5edca496553563008a09af81703ba11e166cdd24
ddebnam <djdebnam@gmail.com>
2022-06-28 12:29:24 -0400

    changed login box-shadow effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import CreateAccount from "./components/CreateAccount";
@@ -9,10 +9,10 @@ function App() {
     <div className="App" id="app-body">
       <Router>
         <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/dashboard" element={<Dashboard/>}/>
-          <Route path="/createaccount" element={<CreateAccount/>}/>
-        </Routes> 
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/createaccount" element={<CreateAccount />} />
+        </Routes>
       </Router>
     </div>
   );
