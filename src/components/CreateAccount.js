d80b44e200610cfaa43f96a6eefec93b146a12ee
DJ <djdebnam@gmail.com>
2022-06-29 06:36:42 -0400

    trying a different style for account creation

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -9,8 +9,8 @@ const CreateAccount = () => {
       <div className="overlay"></div>
 
       <div id="create-container">
-        <h2 id="create-header">Welcome!</h2>
-        <h4 id="create-subheader">Let's get started!</h4>
+        <h2 id="create-header">Create an Account</h2>
+        {/* <h4 id="create-subheader">Let's get started!</h4> */}
         <div id="create-email-title">Email</div>
         <input id="create-email" type="text"></input>
         <div id="create-password-title">Password</div>
@@ -18,8 +18,8 @@ const CreateAccount = () => {
         <div id="create-password-title2">Re-type Password</div>
         <input id="create-password2" type="password"></input>
         <Link to="/dashboard/home">
-          <button id="create-button" className="primary-color-button">
-            Create Account
+          <button id="create-button" className="secondary-color-button">
+            Continue
           </button>
         </Link>
       </div>
