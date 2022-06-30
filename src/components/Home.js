58159b52c854c962103a121bfeac234f752059b1
ddebnam <djdebnam@gmail.com>
2022-06-30 16:52:11 -0400

    added a search box area to the home view

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,13 @@ const Home = () => {
   return (
     <div>
       <div className="info-column">
-        <div className="info-header"></div>
+        <div className="info-header">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-box"
+          ></input>
+        </div>
         <div className="info-header-separator"></div>
       </div>
     </div>
